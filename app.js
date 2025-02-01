adf007e104f632d7016dc69f04d752ec1c15cdb7
wahyunf354 <wahyunurfadillah313@gmail.com>
2025-02-01 17:58:42 +0700

    update select yang bisa disearch

--- app.js
+++ app.js
@@ -5,52 +5,59 @@ const selectCategoryEl = document.getElementById("select-categories");
 const budgeFilterEl = document.getElementById("budge-filter-category");
 const btnReset = document.getElementById("btn-reset");
 
+const ulDropdownCategoryElement = document.getElementById("dropdownList");
+const inputSelectCategoyElement = document.getElementById("searchInput");
+
 document.addEventListener("DOMContentLoaded", async () => {
   const categories = await fetchCategories();
 
   categories.forEach((category) => {
-    const option = document.createElement("option");
-    option.value = category.id;
-    option.dataset;
-    option.textContent = category.name;
-    option.dataset.description = category.name;
-    selectCategoryEl.appendChild(option);
-  });
-
-  selectCategoryEl.addEventListener("change", (e) => {
-    const selectedOption = e.target.options[e.target.selectedIndex];
-
-    if (categoriesSelects.includes(selectedOption.value)) {
-      return;
-    }
-
-    const badgeEl = document.createElement("div");
-    badgeEl.className = "badge badge-primary mx-1 cursor-pointer";
-    badgeEl.setAttribute("data-id", selectedOption.value);
-    badgeEl.onclick = () => {
-      handleRemoveBadge(selectedOption.value);
-    };
-    badgeEl.innerHTML = `
-		  <svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				class="inline-block h-4 w-4 stroke-current">
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M6 18L18 6M6 6l12 12"></path>
-			</svg>
-		${selectedOption.dataset.description}`;
-
-    categoriesSelects.push(selectedOption.value);
-    budgeFilterEl.appendChild(badgeEl);
+    const liEl = document.createElement("li");
+    liEl.textContent = category.name;
+    liEl.onclick = () => selectOption(category.name, category.id);
+    ulDropdownCategoryElement.appendChild(liEl);
   });
 
   btnFilter.addEventListener("click", handleDisplayIrisanToken);
   btnReset.addEventListener("click", resetFilter);
 });
+
+function selectOption(value, id) {
+  console.log("Value", value);
+  inputSelectCategoyElement.value = value;
+  document.getElementById("dropdownList").style.display = "none";
+  handleChangeInputSelectOption(value, id);
+}
+
+function handleChangeInputSelectOption(value, id) {
+  console.log("Change", value, id);
+  if (categoriesSelects.includes(value)) {
+    return;
+  }
+
+  const badgeEl = document.createElement("div");
+  badgeEl.className = "badge badge-primary mx-1 cursor-pointer";
+  badgeEl.setAttribute("data-id", id);
+  badgeEl.onclick = () => {
+    handleRemoveBadge(id);
+  };
+  badgeEl.innerHTML = `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      class="inline-block h-4 w-4 stroke-current">
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M6 18L18 6M6 6l12 12"></path>
+    </svg>
+  ${value}`;
+
+  categoriesSelects.push(id);
+  budgeFilterEl.appendChild(badgeEl);
+}
 
 function handleRemoveBadge(categoryId) {
   const badgeToRemove = document.querySelector(`div[data-id="${categoryId}"]`);
@@ -63,7 +70,6 @@ function handleRemoveBadge(categoryId) {
     categoriesSelects.splice(index, 1);
   }
 
-  console.log("Updated categoriesSelects:", categoriesSelects);
   handleDisplayIrisanToken();
 }
 
@@ -82,11 +88,9 @@ async function handleDisplayIrisanToken() {
 
   for (let i = 0; i < categoriesSelects.length; i++) {
     const categoryId = categoriesSelects[i];
-    console.log("Fetching tokens for Category ID:", categoryId);
 
     try {
       const tokens = await fetchTokensByCategory(categoryId);
-      console.log(`Tokens for Category ${categoryId}:`, tokens);
       allTokensByCategory.push(tokens); // Simpan token dari kategori ini
     } catch (error) {
       console.error("Error fetching tokens:", error);
@@ -110,8 +114,6 @@ async function handleDisplayIrisanToken() {
     null
   );
 
-  console.log("Intersected Tokens:", intersectedTokens);
-
   // Tampilkan token yang beririsan
   if (intersectedTokens && intersectedTokens.length > 0) {
     displayTokens(intersectedTokens);
@@ -122,7 +124,6 @@ async function handleDisplayIrisanToken() {
 }
 
 async function fetchCategories() {
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const apiUrl = "https://api.coingecko.com/api/v3/coins/categories";
   const response = await fetch(apiUrl);
   const data = await response.json();
@@ -130,9 +131,7 @@ async function fetchCategories() {
 }
 
 async function fetchTokensByCategory(categoryId) {
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=${categoryId}&order=market_cap_desc`;
-  console.log("Fetching from:", apiUrl); // Debug: Check the full URL
 
   const response = await fetch(apiUrl);
   if (!response.ok) {
@@ -231,6 +230,4 @@ function resetFilter() {
   }
 
   tokenListContainer.innerHTML = "";
-
-  console.log("Filter has been reset");
 }
